7d4f9b2d5a5fc9670992486d23aa83345edcef4f
Yureyko228 <60176477+Yureyko228@users.noreply.github.com>
2020-05-14 01:00:44 +0300

    [yura] add block rules

---
+++ main.js
@@ -0,0 +1,16 @@
+//не трогать , потом переделаю
+function moreText() {
+    let dots = document.getElementById("dots");
+    let moreText = document.getElementById("more");
+    let btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
