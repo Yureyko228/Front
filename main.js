a055301a7ab76a998be7463ed02c43584b5e6d71
Yureyko228 <60176477+Yureyko228@users.noreply.github.com>
2020-05-15 15:25:06 +0300

    [yura] v0.0.1

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-//не трогать , потом переделаю
-function moreText() {
-    let dots = document.getElementById("dots");
-    let moreText = document.getElementById("more");
-    let btnText = document.getElementById("myBtn");
-
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btnText.innerHTML = "Read more";
-        moreText.style.display = "none";
-    } else {
-        dots.style.display = "none";
-        btnText.innerHTML = "Read less";
-        moreText.style.display = "inline";
-    }
+function visibleEdit(){
+    document.querySelector(".edit-profile").style.display="flex";
+    document.querySelector(".profile").style.display="none";
+    console.log("123");
 }
